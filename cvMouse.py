161564743b4221fa2dbe2ce2b59ed7f4c6029752
Franklin Sun <42247197+fsun71@users.noreply.github.com>
2020-04-26 00:34:34 -0400

    Add files via upload

--- cvMouse.py
+++ cvMouse.py
@@ -1,17 +1,19 @@
 import numpy as np
 import cv2 as cv
 import pyautogui
+import time
 
 pyautogui.FAILSAFE = False
 cap = cv.VideoCapture(0)
+initTime = time.time()
 
-def setThreshold(frame):
+def setThreshold(frame, xyMatrix):
     rows, cols, _ = frame.shape
 
-    y1 = int(cols * 0.60)
-    y2 = int(cols)
-    x1 = int(rows * 0.20)
-    x2 = int(rows * 0.60)
+    y1 = int(cols * xyMatrix[0])
+    y2 = int(cols * xyMatrix[1])
+    x1 = int(rows * xyMatrix[2])
+    x2 = int(rows * xyMatrix[3])
 
     frame = frame[x1:x2, y1:y2]
 
@@ -23,8 +25,8 @@ def setThreshold(frame):
 
     return thresh
 
-def fingerDetect(frame):
-    contours, hierarchy = cv.findContours(frame.copy(), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+def generateTrackpad(frame):
+    contours, hierarchy = cv.findContours(frame.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
     c = max(contours, key=cv.contourArea)
     extTop = tuple(c[c[:, :, 1].argmin()][0])
 
@@ -47,29 +49,65 @@ def fingerDetect(frame):
         if extTop[0] <= trackpadBR[0] and extTop[1] <= trackpadBR[1]:
             cursorTL = (extTop[0]-2, extTop[1]-2)
             cursorBR = (extTop[0]+2, extTop[1]+2)
-        else:
-            cursorTL = (trackpadBR[0]-2, trackpadBR[1]-2)
-            cursorBR = (trackpadBR[0]+2, trackpadBR[1]+2)
-    else:
-        cursorTL = (trackpadTL[0]-2, trackpadTL[1]-2)
-        cursorBR = (trackpadTL[0]+2, trackpadTL[1]+2)
     
-    cv.rectangle(colorFrame, cursorTL, cursorBR, (0, 255, 0), 2)
+    cv.rectangle(colorFrame, cursorTL, cursorBR, (0, 0, 255), 2)
 
     cursorLoc = (10*((cursorTL[0]+2) - trackpadTL[0]), 10*((cursorTL[1]+2) - trackpadTL[1]))
 
     return colorFrame, cursorLoc
 
+def generateBtn(frame):
+    contours, hierarchy = cv.findContours(frame.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    c = max(contours, key=cv.contourArea)
+    extRight = tuple(c[c[:, :, 0].argmax()][0])
+
+    colorFrame = cv.cvtColor(frame, cv.COLOR_GRAY2RGB)
+
+    cols, rows, _ = colorFrame.shape
+
+    trackpadFrameWidth = 80
+    trackpadFrameHeight = 80
+
+    trackpadTL = (int((rows - trackpadFrameWidth)/2), int((cols - trackpadFrameHeight)/2))
+    trackpadBR = (int(rows - (rows - trackpadFrameWidth)/2), int(cols - (cols - trackpadFrameHeight)/2))
+
+    cv.rectangle(colorFrame, trackpadTL, trackpadBR, (255, 0, 255), 3)
+
+    cursorTL = (trackpadTL[0]-2, trackpadTL[1]-2)
+    cursorBR = (trackpadTL[0]+2, trackpadTL[1]+2)
+
+    cursorClick = False
+    cursorColorIndic = (0, 255, 0)
+
+    if extRight[0] >= trackpadTL[0] and extRight[1] >= trackpadTL[1]:
+        if extRight[0] <= trackpadBR[0] and extRight[1] <= trackpadBR[1]:
+            cursorTL = (extRight[0]-2, extRight[1]-2)
+            cursorBR = (extRight[0]+2, extRight[1]+2)
+            cursorColorIndic = (0, 255, 0)
+            cursorClick = True
+    
+    cv.rectangle(colorFrame, cursorTL, cursorBR, cursorColorIndic, 8)
+
+    return colorFrame, cursorClick
+
 def main():
     while(True):
         ret, frame = cap.read()
+        currTime = time.time()
 
         image = cv.flip(frame, 90)
+        trackpadXY = [0.6, 1.0, 0.2, 0.6]
+        buttonXY = [0.7, 0.9, 0, 0.2]
 
-        trackpadDisplay, cursorLoc = fingerDetect(setThreshold(image))
+        trackpadDisplay, cursorLoc  = generateTrackpad(setThreshold(image, trackpadXY))
+        buttonDisplay, cursorClick = generateBtn(setThreshold(image, buttonXY))
 
-        cv.imshow('Optical Trackpad v 0.1b', trackpadDisplay)
+        cv.imshow('Optical Trackpad', trackpadDisplay)
+        cv.imshow('Optical Mouse Buttons', buttonDisplay)
         pyautogui.moveTo(cursorLoc[0], cursorLoc[1])
+
+        if cursorClick and (currTime - initTime >= 4):
+        	pyautogui.click()
 
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
